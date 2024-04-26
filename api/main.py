616d9def483f0279824500fd5845392cead8313a
Alexander Rubin <your@email.here>
2024-04-26 08:53:02 -0400

    the notes are lighting up

--- api/main.py
+++ api/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from .routers import (
-    template,
     notes
 )
 
 
 
 app = FastAPI()
-app.include_router(template.router, tags=["template"])
 app.include_router(notes.router, tags=["notes"])
 
 
--- api/models/template.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Union, Optional
-
-class Item(BaseModel):
-    name: Optional[str]
-    price: Optional[float]
-    is_offer: Optional[Union[bool, None]] = None
